48761248d8216ab478127efa3cbc0cecae89714e
Domenico DiNicola <dom.dinicola@gmail.com>
2026-09-22 20:35:47 +0300

    Harden session cookie: 1-day lifetime, expire at browser close

--- src/hope_live/config/__init__.py
+++ src/hope_live/config/__init__.py
@@ -56,6 +56,8 @@ DEFAULTS = {
     "SENTRY_ENABLE_TRACING": (bool, False, False, "", ""),
     "SENTRY_URL": (str, ""),
     "SESSION_COOKIE_HTTPONLY": (bool, True),
+    "SESSION_COOKIE_AGE": (int, 86400),
+    "SESSION_EXPIRE_AT_BROWSER_CLOSE": (bool, True),
     "SESSION_COOKIE_NAME": (str, "sessionid"),
     "SECURE_HSTS_INCLUDE_SUBDOMAINS": (bool, True),
     "SECURE_HSTS_PRELOAD": (bool, True),
